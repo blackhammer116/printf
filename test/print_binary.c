f0beae33030244988371cdc8c6896457582a66e9
Nebiyu <nebiyuhiyabu@gmail.com>
2022-10-14 15:34:40 -0700

    decimal to binary converter

---
+++ test/print_binary.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_binary - converts a givin decimal number into binary
+ * representation.
+ * @num: decimal parameter which gets converted into 
+ * a binary representation.
+ */
+int print_binary(int num)
+{
+	int i, temp, j = 0;
+	int arr[10000], arr2[10000];
+	int qot;
+	qot = num / 2;
+	for (i = 0; qot != 0; i++)
+	{
+		qot = num / 2;
+		arr[j] = num % 2;
+		num = qot;
+		j++;
+	}
+	i = 0;
+	for (j; j > 0; j--)
+	{
+		temp = arr[j];
+		arr2[i] = temp;
+		i++;
+	}
+	return (arr2);
+}
