499c9bb57e1a1a393febcf84dee9aaa174b4b50f
mikias <mikiasabiy16@gmail.com>
2022-10-15 06:56:37 -0400

    updare: print_hex.c: major bug fixed

--- test/print_hex.c
+++ test/print_hex.c
@@ -1,4 +1,67 @@
-#include "main.c"
+#include "main.h"
+
+/**
+ * rev_string - reverse a given string and store it back to the owner var
+ * @s: The address of the string to be reversed.
+ *
+ * Return: Void.
+ */
+void rev_string(char *s)
+{
+	int i;
+	char *first, *last, holder;
+
+	first = s;
+	last = &(*(s + (_strlen(s) - 1)));
+
+	for (i = 0; i < _strlen(s) / 2; i++)
+	{
+		holder = *last;
+		*last = *first;
+		*first = holder;
+
+		first += 1;
+		last -= 1;
+	}
+}
+/**
+ * convert_to_letter - converts any numb between 10 and 15
+ * to therir respective hex value.
+ * @arr: The array of the numbers.
+ * @len: The length of the array;
+ *
+ * Return: A pointer to the coverted array on error NULL.
+ */
+char *convert_to_letter(int *arr, int len)
+{
+	int i;
+	char *p;
+
+	if (arr == NULL || len == 0)
+		return (NULL);
+	p = malloc(sizeof(char) * len);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i <= len; i++)
+	{
+		if (arr[i] == 10)
+			p[i] = 'A';
+		else if (arr[i] == 11)
+			p[i] = 'B';
+		else if (arr[i] == 12)
+			p[i] = 'C';
+		else if (arr[i] == 13)
+			p[i] = 'D';
+		else if (arr[i] == 14)
+			p[i] = 'E';
+		else if (arr[i] == 15)
+			p[i] = 'F';
+		else
+			p[i] = (arr[i] + '0');
+	}
+	rev_string(p);
+	return (p);
+}
 /**
  * print_hex - convertes decimal numbers into hexadecimal
  *  numbers.
@@ -7,9 +70,8 @@
  */
 int print_hex(int num)
 {
-	int i, temp, j = 0;
-	int arr[10000], arr2[10000];
-	int qot;
+	int i, qot, j, arr[10000];
+	char *arr2;
 
 	qot = num / 16;
 	for (i = 0; qot != 0; i++)
@@ -17,43 +79,9 @@ int print_hex(int num)
 		qot = num / 16;
 		arr[i] = num % 16;
 		num = qot;
-		j++;
 	}
-	i = 0;
-	for (j; j > 0; j--)
-	{
-		switch (arr[j])
-		{
-			case 10:
-				arr[j] = 'A';
-				temp = arr[j];
-				break;
-			case 11:
-				arr[j] = 'B';
-				temp = arr[j];
-				break;
-			case 12:
-				arr[j] = 'C';
-				temp = arr[j];
-				break;
-			case 13:
-				arr[j] = 'D';
-				temp = arr[j];
-				break;
-			case 14:
-				arr[j] = 'E';
-				temp = arr[j];
-				break;
-			case 15:
-				arr[j] = 'F';
-				temp = arr[j];
-				break;
-			default:
-				temp = arr[j];
-				break;
-		}
-		arr2[i] = temp;
-		i++;
-	}
-	return (arr2);
+	arr2 = convert_to_letter(arr, i);
+	_printf("%s", arr2);
+
+	return (i);
 }
